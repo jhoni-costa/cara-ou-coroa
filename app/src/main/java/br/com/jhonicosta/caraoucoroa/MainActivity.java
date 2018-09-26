9fc9e214bae396fcbef2870859e88e48094b20d3
jhoni-costa <jhoni_costa@outlook.com>
2018-09-26 15:55:16 -0300

    Navegação entre Activities

--- app/src/main/java/br/com/jhonicosta/caraoucoroa/MainActivity.java
+++ app/src/main/java/br/com/jhonicosta/caraoucoroa/MainActivity.java
@@ -1,13 +1,30 @@
 package br.com.jhonicosta.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView buttonJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonJogar = findViewById(R.id.buttonJogar);
+
+        buttonJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, ResultadoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/com/jhonicosta/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/br/com/jhonicosta/caraoucoroa/ResultadoActivity.java
@@ -1,13 +1,32 @@
 package br.com.jhonicosta.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView buttonVoltar;
+    private ImageView imageResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        buttonVoltar = findViewById(R.id.buttonVoltar);
+        imageResultado = findViewById(R.id.imageResultado);
+
+        buttonVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ResultadoActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
